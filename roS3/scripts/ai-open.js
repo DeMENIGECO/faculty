bc086302af3fe7ab6335c2cfdd0929343952bbb8
DomeniGeco <domenigeco@gmail.com>
2026-04-24 19:48:27 +0200

    Add files via upload

---
+++ roS3/scripts/ai-open.js
@@ -0,0 +1,3 @@
+function openAi() {
+  window.open("roS3/ai/index.html", "_blank", "width=722,height=780");
+}
---
+++ roS3/scripts/marketplace-open.js
@@ -0,0 +1,3 @@
+function openMarketplace() {
+  window.open("roS3/marketplace/index.html", "_blank", "width=722,height=780");
+}
